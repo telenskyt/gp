05a4e12e24b3852b2dfa1c44753871a6b13adedc
Tomáš Telenský <tomas.telensky@gmail.com>
2025-12-04 15:39:48 +0100

    .

--- R/zzz.R
+++ R/zzz.R
@@ -14,8 +14,9 @@
 	options(warnPartialMatchAttr = TRUE)
 	options(warnPartialMatchArgs = FALSE)
 
-	cat("Here 123!\n")
-	print("We are here")
+	#cat("Here 123!\n")
+	#print("We are here")
+	
 	#library(memoise)
 	environment(memoise_seq_bastl) <<- asNamespace("memoise")
 	#environment(memoise_seq_bastl) <<- new.env(parent = asNamespace("memoise"))
