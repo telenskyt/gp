a8ad8294b61072ea62950e1d8fe56391fa2cdd81
Tomáš Telenský <tomas.telensky@gmail.com>
2025-12-05 20:14:50 +0100

    added comment

--- R/mnfun.R
+++ R/mnfun.R
@@ -1,5 +1,6 @@
 
-
+# h - vector of hyperparameters that are being optimized (not fixed), optim.link funkci musi resit volajici, zde se uz okolo tohoto nic neresi
+# NEBO LIST OF HYPERPARAMETERS??? jako by mel jit do d0, d1, ... i kdyz tam taky zatim nereseno
 mnfun <- function (gp, h)
 {
 	rep(0, gp$GP_size)
