da92c62250b50c6e7c8440ba82f75bdb4bb1ad05
Tomáš Telenský <tomas.telensky@gmail.com>
2025-12-09 11:37:04 +0100

    - new option for whether to rescale f for the likelihood function (and verifying if main table exists in that case) - sanity checks of what the user defined likelihood (and its derivatives) returns. Useful for diagnostics if there are problems with user supplied likelihood function

--- R/gpLikelihood.R
+++ R/gpLikelihood.R
@@ -8,17 +8,22 @@
 
 d0 <- function (gp, f, data = gp$data, hyperpar = numeric(0))
 {
-	if (gp$GP_factor != "1") { 
+	if (gp$ll.rescale_to_main) { 
 		# reindex from the GP_factor to main table 
 		fact_idx <- paste0(gp$GP_factor, "_idx")
 		f <- f[data[[1]][[fact_idx]]]		
 	}
-	gp$ll(data, f)
+	res <- gp$ll(data, f)
+	if (!is.numeric(res) || !all(is.finite(res)))
+		stop("result of user defined likelihood (logLik parameter to gp()) is not numeric: ", res)
+	if (length(res) != 1)
+		stop("length of the result of user defined likelihood (logLik parameter to gp()) is ", length(res), ", should be 1")
+	res
 }
 
 d1 <- function (gp, f, data = gp$data, hyperpar = numeric(0))
 {
-	if (gp$GP_factor != "1") { 
+	if (gp$ll.rescale_to_main) { 
 		# reindex from the GP_factor to main table in the closure wrapper, so that also the automatic differentiation
 		# gives vector of the dimension of GP_factor!
 		cmb <- function(func, data) function(f) {
@@ -35,13 +40,18 @@ d1 <- function (gp, f, data = gp$data, hyperpar = numeric(0))
 	F <- MakeTape(cmb(gp$ll, data), numeric(gp$GP_size))
 	#mstop(id = "MakeTape")
 	
-	drop(F$jacobian(f))
+	res <- drop(F$jacobian(f))
+	if (!is.numeric(res) || !all(is.finite(res)))
+		stop("first derivative of the user defined likelihood (logLik parameter to gp()) is not numeric: ", res)
+	if (length(res) != gp$GP_size)
+		stop("length of the first derivative the of user defined likelihood (logLik parameter to gp()) is ", length(res), ", should be ", gp$GP_size)
+	res	
 }
 
 
 d2 <- function (gp, f, data = gp$data, hyperpar = numeric(0))
 {
-	if (gp$GP_factor != "1") { 
+	if (gp$ll.rescale_to_main) { 
 		# reindex from the GP_factor to main table in the closure wrapper, so that also the automatic differentiation
 		# gives vector of the dimension of GP_factor!
 		cmb <- function(func, data) function(f) {
@@ -59,12 +69,18 @@ d2 <- function (gp, f, data = gp$data, hyperpar = numeric(0))
 	#mstop(id = "MakeTape")
 	
 	F2 <- F$jacfun()$jacfun(sparse = TRUE) # Kasper's advice on how to calculate just Hessian diagonal! https://groups.google.com/g/tmb-users/c/fAaEhwW1niU
-	diag(F2(f))	
+	res <- diag(F2(f))	
+	if (!is.numeric(res) || !all(is.finite(res)))
+		stop("second derivative of the user defined likelihood (logLik parameter to gp()) is not numeric: ", res)
+	if (length(res) != gp$GP_size)
+		stop("length of the second derivative the of user defined likelihood (logLik parameter to gp()) is ", length(res), ", should be ", gp$GP_size)
+	res	
+
 }
 
 d3 <- function (gp, f, data = gp$data, hyperpar = numeric(0))
 {
-	if (gp$GP_factor != "1") { 
+	if (gp$ll.rescale_to_main) { 
 		# reindex from the GP_factor to main table in the closure wrapper, so that also the automatic differentiation
 		# gives vector of the dimension of GP_factor!
 		cmb <- function(func, data) function(f) {
@@ -82,8 +98,12 @@ d3 <- function (gp, f, data = gp$data, hyperpar = numeric(0))
 	#mstop(id = "MakeTape")
 	
 	F3 <- F$jacfun()$jacfun(sparse = TRUE)$jacfun(sparse = TRUE) # analogy of d2()
-	diag(F3(f))
-	
+	res <- diag(F3(f))
+	if (!is.numeric(res) || !all(is.finite(res)))
+		stop("third derivative of the user defined likelihood (logLik parameter to gp()) is not numeric: ", res)
+	if (length(res) != gp$GP_size)
+			stop("length of the third derivative the of user defined likelihood (logLik parameter to gp()) is ", length(res), ", should be ", gp$GP_size)
+	res		
 }
 
 # hyperparametry  pro likelihood zatim vubec neresim
