376f52e6ed04f1cc81b73b9d653741f0d85b15e0
Tomáš Telenský <tomas.telensky@gmail.com>
2025-12-09 11:46:45 +0100

    - new option for whether to rescale f for the likelihood function (and verifying if main table exists in that case)

--- R/gp.R
+++ R/gp.R
@@ -3,7 +3,10 @@
 #'
 #' @param f formula for the covariance matrix
 #' @param data object of class gpData, created by the gpData() function
-#' @param logLik blikelihood function taking two parameters: `data` (object of class gpData), and `f`, a numeric vector - result of the gaussian process
+#' @param logLik likelihood function taking two parameters: `data` (object of class gpData), and \code{f}, a numeric vector - result of the gaussian process.
+#'			The \code{f} vector can be either of the dimension of the gaussian process, or the main table - see the \code{logLik.rescale} parameter.
+#' @param logLik.rescale should the \code{f} parameter of the \code{logLik} function be rescaled to the dimension of the main table?
+#'			If \code{FALSE}, the \code{f} parameter will be kept at the dimension of the Gaussian Process (as reported by \code{gpDetermineSize()}).
 #'
 #' @returns object of class gp - the gaussian process model. Some important slots:
 #'\describe{
@@ -12,7 +15,7 @@
 #'  \item{$data}{training data prepared for model fit - converted to matrices, and scaled (standardized) where appropriate}
 #'}
 #' @export
-gp <- function(f, data, logLik)
+gp <- function(f, data, logLik, logLik.rescale = TRUE)
 {
 	gp <- gpFormula(f)
 	gp$covFormula <- f
@@ -35,7 +38,12 @@ gp <- function(f, data, logLik)
 
 	stopifnot(is.function(logLik))
 	gp$ll <- logLik
-
+	gp$ll.rescale_to_main <- FALSE
+	if (logLik.rescale && gp$GP_factor != "1") { # rescaling is desired to take place
+		if (!gpDataHasMainTable(gp$data))
+			stop("The parameter logLik.rescale = TRUE, but I cannot rescale from the factor ", gp$GP_factor, ", at which gaussian process is running, to the main table, since it is missing in the training data. Supply the main table or consider setting logLik.rescale = FALSE.")
+		gp$ll.rescale_to_main <- TRUE
+	}
 
 	gp
 }
