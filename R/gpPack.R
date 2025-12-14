e1f5d7d145548c7ec565503965450b56a4901410
Tomáš Telenský <tomas.telensky@gmail.com>
2025-12-14 16:09:46 +0100

    Created separate internal function gpPackFit() to pack just the ; Created help for gpPack() and gpUnpack().

--- R/gpPack.R
+++ R/gpPack.R
@@ -1,26 +1,47 @@
 
-#' zahod vse co se da "snadno" vypocitat
+#' Pack the model - reduce its size for memory or disk storage
+#'
+#' Reduce model size by deleting auxiliary data structures which speed up the computations 
+#' but take a lot of space.
+#'
+#' @param maximum should it be taken to maximum level? Default FALSE.
 #' @export
-gpPack <- function(gp)
+gpPack <- function(gp, maximum = FALSE)
 {
-	gp$fit$L <- NULL
-	gp$fit$K <- NULL
-	gp$fit$W <- NULL
+	gp$fit <- gpPackFit(gp$fit, maximum = maximum)
 	gp$data <- NULL
-
-	#gp$fit$f <- NULL # nakonec i bez tohoto se da obejit!!! :-))) # ale necham tam radsi uplne to puvodni, kvuli ruzne num nestabilite....
-	rownames(gp$fit$f) <- NULL # rownames take a lot of space! Just one column matrix with them 279872 bytes, without 31440 bytes!!!
-							# And they only had 6 characters each! (nrow = 3881, ncol = 1). R is not very efficient storing them.
-							# And rownames are in obsx already
-
 	gp
 }
 
-# compute - priprava na vypocty - predikce. Je ale casove narocna viz vyse. compute = FALSE nebude delat ty casove narocne veci...
-# need.K, need.L - v pripade compute = TRUE, pokud nepotrebuju nekterou z techto matic, lze to dat vedet a vyrazne to usetri pamet.
-#	K se ale pocita vzdy (pro vypocet MAP) a jeji attributy (comp_means) zustanou i kdyz need.K = FALSE
-#		- tak zamer s usetrenim pameti se moc nekona, protoze samotne vytvareni matice K_matrix() potrebuje (pro bigmatrix 9471x9471) whole process: 2703.5Mb
-#			- musely by se udelat optimalizace navrzene v komentarich fce K_matrix v K-tsc_spat2.R, ale to by melo byt snadne
+# internal function
+#
+# maximum - should it be the max level of packing
+# pack just the gp$fit object
+gpPackFit <- function(fit, maximum = FALSE)
+{
+	fit$L <- NULL
+	fit$K <- NULL
+	fit$W <- NULL
+	if (maximum) 
+		fit$f <- NULL # nakonec i bez tohoto se da obejit!!! :-))) # ale defaultne necham tam radsi uplne to puvodni, kvuli ruzne num nestabilite....
+	else
+		rownames(fit$f) <- NULL # rownames take a lot of space! Just one column matrix with them 279872 bytes, without 31440 bytes!!!
+							# And they only had 6 characters each! (nrow = 3881, ncol = 1). R is not very efficient storing them.
+							# And rownames are in obsx already
+	fit
+}
+
+#' Unpack the model - prepare it for computations (mostly predictions)
+#'
+#' Restore the auxiliary data structures, which are redundant and take significant space, but are needed for computations.
+#' Note that restoring these structures can take some CPU time.
+#'
+#' @param compute logical - should we prepare for computations (mostly predictions)? This is the CPU-heavy part. \code{compute = FALSE} will skip this.
+#'
+#' @param need.K,need.L logical - in case of \code{compute = TRUE}, specify whether particular matrix (K, L) is needed. If only one of these matrices (K, L) is needed, it can 
+#' significantly save memory, and in case of L matrix, also CPU time. Note that if \code{compute = TRUE}, \code{K} matrix will be calculated regardless of \code{need.K};
+#' so \code{need.K = FALSE} will only save the memory size of the resultant object, but not CPU time.
+#'
 #' @export
 gpUnpack <- function (gp, compute = TRUE, need.K = compute, need.L = compute)
 {
