e9f05d1d8b1137f36eb6dc9cda92ef141bf51f32
Tomáš Telenský <tomas.telensky@gmail.com>
2025-12-30 18:33:37 +0100

    First implementation of my enhanced foreach(), taken from the legacy atlas code. This first implementation of %dopar2% doesn't currently work (in the simple test it says Error: object 'i' not found).

--- R/functions.R
+++ R/functions.R
@@ -107,3 +107,143 @@ endLog <- function (conn)
 	}
 }
 
+
+#' Get the true working directory
+#'
+#' Like getwd(), but keeps the original path in case it contains symlinks on UNIX systems (doesn't replace symlinks in the path).
+#' @export
+getwd.true <- function()
+{
+	if (.Platform$OS.type == "unix") {
+		wd <- Sys.getenv('PWD')
+		stopifnot(wd != "")
+	} else {
+		wd <- getwd()
+	}
+	wd
+}
+
+
+
+
+expand_special <- function (fn)
+{
+	hostname <- Sys.info()["nodename"]
+	#hostname.abbrev <- sub("telensky-vypocty-?", "", hostname)
+	#if (hostname.abbrev == "") hostname.abbrev <- "1"	
+	#fn <- gsub("%h", hostname.abbrev, fixed = TRUE, fn)
+	fn <- gsub("%H", hostname, fixed = TRUE, fn)
+	fn <- gsub("%p", Sys.getpid(), fixed = TRUE, fn)
+	fn
+}
+
+
+
+#' Wrapper for parallel/background jobs
+#'
+#' Useful wrapper for parallel jobs, run by e.g. foreach(), or, in general, for any non-interactive (background) jobs. 
+#' It will make sure the working directory is set correctly, standard and error outputs are logged, and in case of 
+#' error, a dump file will be saved for debugging purposes.
+#'
+#' @param parallel Should be set to \code{TRUE} for the wrapper to work normally. The \code{FALSE} setting is just for some perhaps
+#' debugging purposes, when we want to run the job in non-parallel, interactive mode, in which case most of the wrapper function (logging, setting working directory)
+#' will be disabled.
+#'
+#' @param working.dir working directory
+#' @param masterPID the PID (process ID) of the process that is dispatching this parallel computation
+#' @param log.fn filename of the standard and error output log file
+#' @param dump.fn filename for the debug dump upon an error - without the .rda extension, that one will be added
+#' @export
+parallelJobWrapper <- function (parallel = TRUE, working.dir = NULL, masterPID = NULL, log.fn = "log-%h_%p.txt", dump.fn = "dump-%h_%p", expr)
+
+#myParallel
+#jobWrapper
+#bgJobWrapper
+#bgJob
+#parallelJob
+#parallelJobWrapper
+#parJobWrapper
+
+{
+	res <- withCallingHandlers({
+		if (parallel) {
+			hostname <- Sys.info()["nodename"]
+			if (!is.null(working.dir)) {
+				setwd(working.dir)
+			}						
+			# spust logovani - dej logu do jmena hostname a PID, kdyby nahodou na tom samem tasku makalo vic workeru (u doRedis clovek nikdy nevi!)
+			if (!is.null(log.fn)) {
+				log.fn <- expand_special(log.fn)
+				fold.LOG <- startLog(filename = log.fn, add_timestamp = TRUE)
+				on.exit({ cat("closing log file\n"); endLog(fold.LOG); cat("(after closing log, shouldn't be seen in any log)\n"); }) 
+					# we have to do it like this, because of the doRedis bug (worker will not close the sink; po case to zpusobi chybu "sink stack is full". Not reported yet)
+			}
+			cat("Running in parallel: my PID = ", Sys.getpid(), " at ", hostname, "; masterPID = ", masterPID, "\n")
+			cat("Starting at the following memory: gc(reset = TRUE):\n")
+			message("testing stderr (this goes to message connection, which is like stderr)\n")
+			print(gc(reset = TRUE))
+			cat("\n\n")
+		}
+		
+		expr
+	}, error = function (e) {
+		cat("error: ")
+		print(e)
+		#cat("\n")
+		if (!is.null(dump.fn)) {
+			dump.fn <- expand_special(dump.fn)
+			cat("dumping stack, variables etc. to ", dump.fn, "\n")
+			dump.frames(dumpto = dump.fn, to.file = TRUE, include.GlobalEnv = TRUE) # use debugger(loadVar("*", dump.fn)) to debug...
+			#cat("The stack:\n") # nevim jak ho vypsat, nic nefacha:
+			#recover() # nic nevypise
+			#browser() # nic nevypise
+			#traceback() # vyhodi no traceback available! # trochu me stve, ze to nedava ten samy stack format jako recover()... mozna todo: vykuchat to z recover()...
+		}
+	})
+	gc() # free up the memory because e.g. doRedis workers don't call gc() after it finishes (they should!!)
+	return(res)
+}
+
+#' Enhanced version of foreach()
+#' 
+#' Enhanced version of \code{foreach()} that takes care of setting correct working directory, logging standard and error output to a log file, and dumping debug info upon error.
+#' 
+#' @param .pass.wd should the worker process set the same working directory as is on the master, before the job is executed? In most cases, \code{TRUE} will be the desired value (default).
+#'		Keep in mind that in some setups, e.g. with \code{library(doRedis)}, one might have the workers running on different machine.
+#' @param .working.dir working directory to be set before the job is executed. Will override \code{.pass.wd} if set.
+#' @param ... arguments passed to \link[foreach]{foreach}. Or \code{\link[foreach]{foreach}}. Or \code{foreach()}.
+#' @export
+foreach2 <- function (.parallel = TRUE, .pass.wd = TRUE, .working.dir = NULL, .log.fn = "log-%h_%p.txt", .dump.fn = "dump-%h_%p", ...)
+{
+	obj <- foreach(...)
+	masterPID <- Sys.getpid()
+	if (is.null(.working.dir) && .pass.wd) {
+		.working.dir <- getwd.true()
+	}
+	obj$foreach2 <- list(
+		parallel = .parallel,
+		working.dir = .working.dir,
+		masterPID = masterPID,
+		log.fn = .log.fn,
+		dump.fn = .dump.fn
+	)
+	obj
+}
+
+#' Enhanced version of `%dopar%`
+#' 
+#' 	Enhanced version of \code{\%dopar\%} that works with \code{foreach2()} and takes care of setting correct working directory, logging standard and error output to a log file, and dumping debug info upon error.
+#' @export
+`%dopar2%` <- function (obj, ex)
+{
+	args <- obj$foreach2
+	if (args$parallel) 
+		`%do_as_needed%` <- `%dopar%`
+	else
+		`%do_as_needed%` <- `%do%`
+	args <- c(args, list(expr = ex))
+
+	obj %do_as_needed% {
+		do.call(parallelJobWrapper, args)
+	}
+}
