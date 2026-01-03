688af1463e12f3e54bfaf7d66c7be4d66516789c
Tomáš Telenský <tomas.telensky@gmail.com>
2026-01-03 20:31:26 +0100

    fixed a problem in gpFitCV(): - set a clear criteria of how fold.fact must relate to gp$GP_factor - introduced reindexing of the fold.fact to gp$GP_factor when needed!

--- R/gpFitCV.R
+++ R/gpFitCV.R
@@ -7,6 +7,7 @@
 #'		The column (or the supplied vector) must be a vector of integers from \code{1} to \code{N} (\code{N} being the number of folds), 
 #'		specifying the number of the cross-validation fold the given record belongs to.
 #' @param fold.fact a factor along which the cross-validation folds (\code{fold.col}) are specified. Factor \code{"1"} means the folds are specified for the rows of the main table.
+#'		Note that if the GP dimension is given by some real grouping factor (i.e. \code{p$GP_factor != "1"}), then the \code{fold.fact} must be that factor.
 #' @param folds integer vector of folds to fit; if \code{NULL} (the default), all folds are fit
 #' @param start.from.model an object of class \code{gp} to take the starting values from. If specified, then for each cross-validation fold model to be fit, 
 #' 		the starting values of the hyperparameters will be taken from the corresponding fold model wherever possible.
@@ -31,6 +32,9 @@ gpFitCV <- function (gp, fold.col, fold.fact = "1", folds = NULL, start.from.mod
 	parallel = TRUE, fn.prefix = "", log.fn = "log-fold%f-%h-%p.txt", dump.fn = "dump-fold%f-%h_%p",
 	...)
 {
+	if (gp$GP_factor != "1")
+		if (fold.fact != gp$GP_factor)
+			stop("If the factor corresponding to the GP (gp$GP_factor) is some real grouping factor (!= \"1\"), then the fold.fact must be that factor")
 	if (is.character(fold.col) && length(fold.col) == 1) { # it is a name of a column in the main table
 		if (fold.fact != "1")
 			stop("Specifying fold.col by column name only works for fold.fact == '1'")
@@ -77,7 +81,7 @@ gpFitCV <- function (gp, fold.col, fold.fact = "1", folds = NULL, start.from.mod
 	#fold.run <- foreach (f = folds, .packages = c("gp")) %do_as_needed% {
 	#fold.run <- foreach (f = folds, .packages = c("gp", "RTMB")) %do_as_needed% {	
 	fold.run <- foreach (f = folds, .packages = .packages()) %do_as_needed% {	
-		options(warn = 1) # tady veskere options() musi byt znova, protoze na worker/cluster se to neexportuje
+		options(warn = 2) # tady veskere options() musi byt znova, protoze na worker/cluster se to neexportuje
 		options(show.error.locations = TRUE)
 		options(keep.source = TRUE)	
 		
@@ -141,6 +145,16 @@ gpFitCV <- function (gp, fold.col, fold.fact = "1", folds = NULL, start.from.mod
 		stats = NULL # CV stats		
 	)
 	# put the results together
+	# now, reindex the fold.col to the dimension of the prediction (gp$GP_factor)
+	# thanks to the condition above, the only case when reindexing might be needed is when GP_factor = "1" and fold.fact = something else.
+	if (fold.fact != gp$GP_factor) {
+		stopifnot(gp$GP_factor == "1") # consequence of the checks above
+		stopifnot(fold.fact != "1") # consequence of the checks above
+		# now, we have to reindex fold.col to the main table
+		stopifnot(gpDataHasMainTable(gp$obsdata)) # has to have it in this case
+		fold_idx_col <- paste0(fold.fact, "_idx")
+		fold.col <- fold.col[gp$obsdata[[1]][[fold_idx_col]]]
+	}
 	for (i in 1:length(fold.run)) { 
 		f <- fold.run[[i]]$fold
 		gp$fitCV$models[[f]] <- fold.run[[i]]$m
