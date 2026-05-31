5df812f63309e6e7b8b3694f6eb31964eeeb9f22
Tomáš Telenský <tomas.telensky@gmail.com>
2026-05-31 15:33:16 +0200

    - added new gpGetParForLikTemplate() function for the repeated code 	- handles lik.reindex2main parameter of gp(), and adds likelihood hyperparameters

--- R/gpLikelihood.R
+++ R/gpLikelihood.R
@@ -4,4 +4,16 @@
 # obecna note k temto fcim - zatim nechavam data jako separatni parametr, i kdyz by mozna stacilo brat proste data z gp$data
 # (ale co cross-validace treba?)
 
-
+gpGetParForLikTemplate <- function(gp, f, data, hyperpar)
+{
+	if (gp$lik.reindex2main && gp$GP_factor != "1") { 
+		stopifnot(gpDataHasMainTable(data))	
+		# reindex from the GP_factor to main table, as requested by the template
+		# this should be called within the MakeTape template, so that also the automatic differentiation
+		# gives vector of the dimension of GP_factor!
+		fact_idx <- paste0(gp$GP_factor, "_idx")
+		f <- f[data[[1]][[fact_idx]]]
+	}
+	par <- c(hyperpar[[".lik"]], list(f = f))
+	par
+}
