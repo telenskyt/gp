fffcd6fa0c44ecc8b6d381ca8b5a951abd6b439e
Tomáš Telenský <tomas.telensky@gmail.com>
2026-06-08 19:54:56 +0200

    adapting gpPack() and gpUnpack() to the new class interface for likelihood template

--- R/gpPack.R
+++ R/gpPack.R
@@ -20,7 +20,9 @@ gpPack <- function(gp, maximum = FALSE)
 # pack just the gp$fit object
 gpPackFit <- function(fit, maximum = FALSE)
 {
+	fit$LTinv.rW <- NULL
 	fit$L <- NULL
+	fit$rW <- NULL
 	fit$K <- NULL
 	fit$W <- NULL
 	if (maximum) 
@@ -39,12 +41,15 @@ gpPackFit <- function(fit, maximum = FALSE)
 #'
 #' @param compute logical - should we prepare for computations (mostly predictions)? This is the CPU-heavy part. \code{compute = FALSE} will skip this.
 #'
-#' @param need.K,need.L logical - in case of \code{compute = TRUE}, specify whether particular matrix (K, L) is needed. If only one of these matrices (K, L) is needed, it can 
-#' significantly save memory, and in case of L matrix, also CPU time. Note that if \code{compute = TRUE}, \code{K} matrix will be calculated regardless of \code{need.K};
-#' so \code{need.K = FALSE} will only save the memory size of the resultant object, but not CPU time.
+#' @param need.K,need.L,need.LTinv.rW logical - in case of \code{compute = TRUE}, specify whether particular matrix (K, L) is needed. If only one of these matrices (K, L, need.LTinv.rW) is needed, it can 
+#' significantly save memory and sometimes also CPU time. But the object depends on each other (K -> L -> LTinv.rW),
+#' so if you set this to \code{FALSE} for an object on which another needed object depends on,
+#' you will only save the memory size of the resultant object, but not CPU time.
+#'
+#' Note that the status of the \code{rW} object will be exactly the same as the \code{L} object (so it is directed by \code{need.L} argument).
 #'
 #' @export
-gpUnpack <- function (gp, compute = TRUE, need.K = compute, need.L = compute)
+gpUnpack <- function (gp, compute = TRUE, need.K = compute, need.L = FALSE, need.LTinv.rW = compute)
 {
 	gp$data <- gpDataPrepare(gp, gp$obsdata)
 
@@ -54,6 +59,7 @@ gpUnpack <- function (gp, compute = TRUE, need.K = compute, need.L = compute)
 	if (!compute) {
 		stopifnot(!need.K)
 		stopifnot(!need.L)
+		stopifnot(!need.LTinv.rW)
 		return(gp)
 	}
 
@@ -66,13 +72,17 @@ gpUnpack <- function (gp, compute = TRUE, need.K = compute, need.L = compute)
 		stopifnot(!is.null(gf$fit[["f"]]))
 		stopifnot(!is.null(gf$fit[["a"]]))
 		stopifnot(!is.null(gf$fit[["K"]]))
-		stopifnot(!is.null(gf$fit[["L"]]))
 		stopifnot(!is.null(gf$fit[["W"]]))
 		gp$fit$f <- gf$fit$f
 		gp$fit$a <- gf$fit$a
 		gp$fit$K <- gf$fit$K
-		gp$fit$L <- gf$fit$L
 		gp$fit$W <- gf$fit$W
+		if (!is.null(gf$fit[["L"]])) {
+			gp$fit$L <- gf$fit$L
+			gp$fit$rW <- gf$fit$rW
+		}
+		if (!is.null(gf$fit[["LTinv.rW"]]))
+			gp$fit$LTinv.rW <- gf$fit$LTinv.rW
 		return(gp)
 	}
 	
@@ -89,12 +99,16 @@ cat("Re-creating covariance matrix... ")
 
 	mn <- mnfun(gp, hyperpar = hyperpar)
 	#gp$fit$f <- gp$fit$K %*% gp$fit$a + mn
+	
+	fisher <- NULL
+	if (gp$fit$method == "Laplace-Fisher")
+		fisher <- gp$fit$fisher.options
 
-	gp$fit$W <- -(gp$fit$wt * d2(gp, gp$fit$f, gp$data, hyperpar))
+	gp$fit$W <- -d2(gp, gp$fit$f, gp$data, hyperpar, fisher)
 
 	stopifnot(all(gp$fit$W >= 0))
 
-	if (!need.L) {
+	if (!need.LTinv.rW && !need.L) {
 		if (!need.K) {
 			empty_K <- NA
 			mostattributes(empty_K) <- attributes(gp$fit$K)
@@ -103,27 +117,61 @@ cat("Re-creating covariance matrix... ")
 		return(gp)
 	}
 
-	# recompute L
-	mstart(id = "L")
-cat("Re-computing L matrix ... ")
-
-	rW <- sqrt(gp$fit$W)
-	xx <- rW %*% t(rW)# * K + diag(n)
-gc()
-    xx <- xx * gp$fit$K
-	if (!need.K) {
-		empty_K <- NA
-		mostattributes(empty_K) <- attributes(gp$fit$K)
-		gp$fit$K <- empty_K
+	L <- gp$fit[["L"]]
+	rW <- gp$fit[["rW"]]
+	# need.LTinv.rW || need.L
+	if (is.null(gp$fit[["L"]]) && (need.L || is.null(gp$fit[["LTinv.rW"]]))) {
+		# need to recompute L
+		mstart(id = "L")
+		if (gp$W.type == "diag") {
+			rW <- sqrt(gp$fit$W)
+			xx <- rW %*% t(rW)# * K + diag(n)
+			#xx <- rW %*% t(rW) * K + diag(n)
+		gc()
+			xx <- xx * gp$fit$K
+		gc() # ta vlozena gc() tady jsou velmi dulezita!!!
+			xx <- xx + diag(n)
+		gc()
+		} else {
+		gc()
+			rW <- chol_W(gp$fit$W)
+			xx <- rW %*% gp$fit$K %*% t(rW) + diag(n)
+		gc()
+		}
+		if (!need.K) {
+			empty_K <- NA
+			mostattributes(empty_K) <- attributes(gp$fit$K)
+			gp$fit$K <- empty_K
+		}
+		gc() # ta vlozena gc() tady jsou velmi dulezita!!!		
+		L <- chol(xx) # see the note on L above (it's a transpose of L in R&W2006, but it's OK)
+		rm(xx)
+		mstop(id = "L")
+		gc()
+	} else {
+		if (!need.K) {
+			empty_K <- NA
+			mostattributes(empty_K) <- attributes(gp$fit$K)
+			gp$fit$K <- empty_K
+		}
+		gc() # ta vlozena gc() tady jsou velmi dulezita!!!		
 	}
-gc() # ta vlozena gc() tady jsou velmi dulezita!!!
+	if (need.LTinv.rW && is.null(gp$fit[["LTinv.rW"]])) {
+		mstart(id = "LTinv.rW")
+		stopifnot(!is.null(rW))
+		stopifnot(!is.null(L))
+		if (gp$W.type == "diag")
+			LTinv.rW <- backsolve(L, diag(rW), transpose = TRUE) # L^T^-1 chol(W)
+		else
+			LTinv.rW <- backsolve(L, rW, transpose = TRUE) # L^T^-1 chol(W)
+		mstop(id = "LTinv.rW")
+	}
+	if (need.L) {
+		gp$fit$L <- L
+		gp$fit$rW <- rW
+	}
+	if (need.LTinv.rW) 
+		gp$fit$LTinv.rW <- LTinv.rW
 
-	xx <- xx + diag(n)
-gc()
-	gp$fit$L <- tryCatch(chol(xx), error = function(x) return(NULL))
-	rm(xx)
-	mstop(id = "L")
-gc()
-#mstop()
 	gp
 }
