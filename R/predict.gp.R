ac11cfc9ce16c4cb43ec3771080ea3921e7d0a04
Tomáš Telenský <tomas.telensky@gmail.com>
2026-06-09 16:40:07 +0200

    - fixing the bug in predict.gp() in reindexing the predictions - improving the help

--- R/predict.gp.R
+++ R/predict.gp.R
@@ -1,39 +1,39 @@
 #' Predict from a fitted Gaussian process model
-#' 
-#' Predict from a fitted Gaussian process model. 
-#' 
-#' @param newdata object of class gpData, ***unscaled*** (i.e. not passed through \code{gpDataPrepare()}) data! They must be on the same scale as 
+#'
+#' Predict from a fitted Gaussian process model.
+#'
+#' @param newdata object of class gpData, ***unscaled*** (i.e. not passed through \code{gpDataPrepare()}) data! They must be on the same scale as
 #' the unscaled training dataset used for fitting the model. If \code{NULL} (the default), predictions will be made on the training dataset (which
 #' is going to be super fast if CI = NULL)
 #' @param gp object of class gp, the fitted Gaussian process model
 #' @param hyperpar (optional) lists of hyperparameter values, as returned by \code{gpHyperparList()}; default is to use the hyperparameters from the \code{gp} object.
 #' @template param-components
-#' 
-#' @param comp_missing how to compensate for missing components (if there are any), in mean and variance of the predictions: 
+#'
+#' @param comp_missing how to compensate for missing components (if there are any), in mean and variance of the predictions:
 ##' \describe{
 ##' \item{\code{"none"}}{no compensation. In this case, the interpretation of the result's variance isn't much clear (!!!??? why?).}
 ##' \item{\code{"avg"}}{the missing components are replaced by "averaging over them" - more
-##'		precisely, it predicts for the average effects of these components - for these, the variance/CI corresponds to confidence interval 
+##'		precisely, it predicts for the average effects of these components - for these, the variance/CI corresponds to confidence interval
 ##'		(uncertainty around the mean predictions) and not the prediction interval - see http://www.sthda.com/english/articles/40-regression-analysis/166-predict-in-r-model-predictions-and-confidence-intervals/
 ##'		For models which have grouping factors, be aware of how the average is weighted - see CAUTION CAU001 in the code.
 ##' }
 #' }
 #' Thus, the most reasonable setting now is \code{"avg"}; unless one cares only about the prediction mean (`f`) of the latent variable, in which case
 #' a simple "none" would also do the job.
-#' 
-#' @param w providing this optional argument will calculate average prediction(s) of all data points in this dataset, given these weights. Can 
-#' be either a vector of the same length as the size of \code{newdata} (in which case a single average is calculated), or, 
+#'
+#' @param w providing this optional argument will calculate average prediction(s) of all data points in this dataset, given these weights. Can
+#' be either a vector of the same length as the size of \code{newdata} (in which case a single average is calculated), or,
 #' if more different averages with different weights are desired, `w` can be a matrix where each column corresponds to one average - one set of weights
 #' (i.e. number of rows of `w` must be equal to the size of \code{newdata}, number of columns equal to the number of different averages to be calculated).
 #'
 #' @param groupMeans optional, factor of the size of the predicted dataset. If provided, mean predictions for each factor level are calculated.
-#' 
+#'
 #' @param type character; type of prediction:
 #' \describe{
 #' \item{\code{"latent"}}{predictions directly from the Gaussian Process itself (\code{f} and optionally \code{f_SE}), always in the dimension of the GP (\code{gp$GP_factor})}
-#' \item{\code{"terms"}}{predictions including GP (\code{f} and optionally \code{f_SE}) along with all the other terms used in the likelihood template. If gp$lik.reindex2main = TRUE, 
+#' \item{\code{"terms"}}{predictions passed through first stage of the likelihood template, that is, including GP (\code{f} and optionally \code{f_SE}) along with all the other terms used in the likelihood template. If \code{gp$lik.reindex2main} is \code{TRUE},
 #'						the predictions will be reindexed to the dimension of the main table.}
-#' \item{\code{"response"}}{predictions after applying the inverse link function. If gp$lik.reindex2main = TRUE, 
+#' \item{\code{"response"}}{predictions passed through the first two stages of the likelihood template, that is, after applying the inverse link function. If \code{gp$lik.reindex2main} is \code{TRUE},
 #'						the predictions will be reindexed to the dimension of the main table.}
 #' }
 #'
@@ -50,9 +50,9 @@
 #'
 #' @param pred.sims (currently unused) number of simulations to be used for calculating the response scale predictions and their CIs. Default is 100000.
 #' Higher values give more accurate results, but are slower. Ignored if \code{CI = NULL}.
-#' @param Kx.cache optional, object returned by \code{K_cache()} function, to speed up repeated calls to \code{predict.gp()} with the same \code{newdata} by 
+#' @param Kx.cache optional, object returned by \code{K_cache()} function, to speed up repeated calls to \code{predict.gp()} with the same \code{newdata} by
 #' caching parts of the K(training_data, newdata) matrix.
-#' @param Kxx.cache optional, object returned by \code{K_cache()} function, to speed up repeated calls to \code{predict.gp()} with the same \code{newdata} by 
+#' @param Kxx.cache optional, object returned by \code{K_cache()} function, to speed up repeated calls to \code{predict.gp()} with the same \code{newdata} by
 #' caching parts of the K(newdata, newdata) matrix.
 #'
 #' @returns A matrix of predictions, with columns \code{f} and \code{f_SE} (if \code{se.fit = TRUE}). If \code{cov.fit = TRUE}, returns a named list, \code{pred} will be
@@ -66,31 +66,31 @@
 # Note: weighting is done before predicting. Which is same as after, as it is all linear, but probably faster.
 
 #
-# groupMeans - 
+# groupMeans -
 
 # ... - passed to model_expand_predictions()
 
 
 
-predict.gp <- function(gp, newdata = NULL, type = c('latent', 'terms', 'response'),	se.fit = FALSE, cov.fit = FALSE, CI = 0.95, 
-						components = NULL, comp_missing = c("avg", "none"), w = NULL, groupMeans = NULL, 
-						hyperpar = gpHyperparList(gp), 
-						maxn = 2000, pred.sims = 100000, 
-						Kx.cache = NULL, Kxx.cache = NULL, ...)  
+predict.gp <- function(gp, newdata = NULL, type = c('latent', 'terms', 'response'),	se.fit = FALSE, cov.fit = FALSE, CI = 0.95,
+						components = NULL, comp_missing = c("avg", "none"), w = NULL, groupMeans = NULL,
+						hyperpar = gpHyperparList(gp),
+						maxn = 2000, pred.sims = 100000,
+						Kx.cache = NULL, Kxx.cache = NULL, ...)
 {
 	if (is.null(gp$fit))
 		stop("Model object has not been fit yet: you need to call gpFit() first")
 	comp_missing <- match.arg(comp_missing)
 	need <- function (object, x) if (is.null(object[[x]])) stop("Model object is missing the `", x, "` element - try to call gpUnpack() on it")
-	
+
 	need(gp, "data")
 	need(gp, "obsdata") #!!! na co?
-	
+
 	components <- validate_components(gp, components) # model components (components of covariance matrix)
 	all_components_used <- all(names(gp$covComp) %in% components)
-	
+
 	type <- match.arg(type)
-	
+
 	# set up data
 	same <- FALSE
 	if (is.null(newdata)) {
@@ -121,7 +121,7 @@ predict.gp <- function(gp, newdata = NULL, type = c('latent', 'terms', 'response
 
 		x_new <- gpDataPrepare(gp, newdata, scale.as = gp$data) # it will scale the newdata in exactly the way that training data set was
 	}
-	
+
 	# check CI
 	if (!is.null(CI)) {
 		if (CI >= 1 | CI <= 0) {
@@ -132,11 +132,11 @@ predict.gp <- function(gp, newdata = NULL, type = c('latent', 'terms', 'response
 	}
 
 	#if (is.null(maxn)) maxn <- ceiling(gpDataSize(x_new, gp$GP_factor)  / 10) # we are splitting the newdata, not the training dataset; ceiling needed instead of round here
-		
+
 	# first, put up together the latent prediction
 	mstart(id = "whole predict", mem_precise = TRUE)
 	mstart(id = "whole pred", mem_precise = TRUE)
-	pred <- pred(gp, x_new, same = same, hyperpar = hyperpar, components = components, comp_missing = comp_missing, 
+	pred <- pred(gp, x_new, same = same, hyperpar = hyperpar, components = components, comp_missing = comp_missing,
 		w = w, groupMeans = groupMeans, se.fit = se.fit, cov.fit = cov.fit, maxn = maxn, Kx.cache = Kx.cache, Kxx.cache = Kxx.cache
 	)
 	cat("pred() took ")
@@ -145,29 +145,29 @@ predict.gp <- function(gp, newdata = NULL, type = c('latent', 'terms', 'response
 		pred.cov <- pred$cov
 		pred <- pred$pred
 	}
-	# pred is now always a matrix, with cols "f" and optionally also "f_SE" 
-	
+	# pred is now always a matrix, with cols "f" and optionally also "f_SE"
+
 	if (type == "terms" || type == "response") {
-		if (gp$lik.reindex2main && gp$GP_factor != "1") { 
-			stopifnot(gpDataHasMainTable(x_new))	
+		if (gp$lik.reindex2main && gp$GP_factor != "1") {
+			stopifnot(gpDataHasMainTable(x_new))
 			# reindex from the GP_factor to main table, as requested by the template
 			fact_idx <- paste0(gp$GP_factor, "_idx")
-			pred <- pred[x_new[[1]][[fact_idx]]]
+			pred <- pred[x_new[[1]][[fact_idx]],,drop=FALSE]
 			if (cov.fit)
 				stop("type == terms or response used with cov.fit = TRUE and with lik.reindex2main = TRUE; would need to reindex the covariance matrix here, but sounds crazy, reconsider this case")
 		}
 		par <- c(hyperpar[[".lik"]], list(f = pred[,"f"]))
 	}
-	
+
 	if (se.fit && !is.null(CI)) { # add confidence intervals
 		upper <- pred[, "f"] + err * pred[, "f_SE"]
 		lower <- pred[, "f"] - err * pred[, "f_SE"]
 		dCI <- cbind(lower, upper)
 		colnames(dCI) <- c(paste0("f_lower_", round(100 * CI), "CI"), #paste("lower ", round(100 * CI), "% CI", sep = ""),
 						   paste0("f_upper_", round(100 * CI), "CI")) #paste("upper ", round(100 * CI), "% CI", sep = "")
-		pred <- cbind(pred, dCI)							   
+		pred <- cbind(pred, dCI)
 	}
-	
+
 	if (type == "terms") {
 		terms <- gp$lik$terms(data = x_new, par) # we are calling $terms and not $stage(stage = 1), because we want just the terms separately
 		if (!is.null(terms))
@@ -179,10 +179,11 @@ predict.gp <- function(gp, newdata = NULL, type = c('latent', 'terms', 'response
 	}
 	cat("returning memory - gc() took ")
 	mstart(id = "gc")
-	gc() # Return the memory. Important! :-) 
+	gc() # Return the memory. Important! :-)
 	mstop(id = "gc")
 	cat("whole predict() took ")
 	mstop(id = "whole predict")
+	stopifnot(is.matrix(pred))
 	if (!cov.fit)
 		pred
 	else
